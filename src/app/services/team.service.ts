4eb5d63a10ba4253e2cfe575bce7c3292ca55755
Emil Nilsson <emil.nilsson@protonmail.com>
2020-03-29 12:59:29 +0200

    Create service to get teams by season
    Note that this should be replaced by an API call directed specifically
    at getting teams for a season. This is simply implemented like this as
    the API does not yet contain such an endpoint.

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core'
 import { Team, TeamRes } from '@models/team'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { map, filter, flatMap } from 'rxjs/operators'
+import { Season } from '@models/season'
+import { Match } from '@models/match'
+import { SeasonService } from './season.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private seasonService: SeasonService,
+  ) { }
 
   getTeamById(id: number): Observable<Team> {
     return this.http.get<{ team: TeamRes }>(`/teams/${id}`)
@@ -29,5 +35,31 @@ export class TeamService {
   getTeamsByDivision(id: number): Observable<Team[]> {
     return this.http.get<{ teams: TeamRes[] }>(`/divisions/${id}/teams`)
       .pipe(map(teams => teams.teams.map(team => new Team(team))))
+  }
+
+  getTeamsBySeason(season: number): Observable<Team[]> {
+    console.warn("Using workaround!",
+      "Getting teams by matches instead of API call.")
+
+    return this.seasonService.getSeasonById(season)
+      .pipe(
+        flatMap(season => {
+          let teamsInSeason = this.teamsFromMatches(season.matches)
+
+          return this.getTeams()
+            .pipe(map(teams => teams.filter(
+              team => teamsInSeason.includes(team.id)
+            )))
+
+        })
+      )
+  }
+
+  private teamsFromMatches(matches: Match[]): number[] {
+    return [... new Set(
+      matches
+        .map((match) => [match.homeTeam, match.awayTeam])
+        .reduce((a, b) => a.concat(b), [])
+    )]
   }
 }
