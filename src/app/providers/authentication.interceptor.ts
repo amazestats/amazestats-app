243812f2338042d7e1e5eb0bb2c8a515d7082295
Emil Nilsson <emil.nilsson@protonmail.com>
2020-07-30 01:00:24 +0200

    Wipe use details when response status is 401
    If we receive a 401 response it means we are unauthenticated. Apart from
    redirecting the user to `/login` we also need wipe the user details
    (token, token expiration, and user id). If this is not done, the user
    would still think it is authenticated, only redirected to `/home`.

--- src/app/providers/authentication.interceptor.ts
+++ src/app/providers/authentication.interceptor.ts
@@ -9,11 +9,13 @@ import { Observable, of, throwError } from 'rxjs';
 import { AuthenticationService } from '@services/authentication.service';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
+import { StorageService } from '@services/storage.service';
 
 export class AuthenticationInterceptor implements HttpInterceptor {
 
   constructor(
     private auth: AuthenticationService,
+    private storageService: StorageService,
     private router: Router,
   ) { }
 
@@ -44,6 +46,7 @@ export class AuthenticationInterceptor implements HttpInterceptor {
 
   private handleError(err: HttpErrorResponse): Observable<any> {
     if (err.status === 401 || err.status === 403) {
+      this.storageService.clearUserDetails()
       this.router.navigateByUrl('/login')
       return of(err.message)
     }
