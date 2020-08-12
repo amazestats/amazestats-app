1304d551c8f87a8c9f271be3db324f2bbc58195b
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-12 23:39:54 +0200

    Create service to get admins

---
+++ src/app/services/competition.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+type AdminList = { id: number, alias: string }[]
+
+const DEFAULT_COMPETITION = 1
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompetitionService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAdmins(
+    competitionId: number = DEFAULT_COMPETITION
+  ): Observable<AdminList> {
+    return this.http.get<{ admins: AdminList }>
+      (`/competitions/${competitionId}/admins`)
+      .pipe(map(res => res.admins))
+  }
+}
