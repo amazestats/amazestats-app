2cf3cabafd3b0e6b98b30622f2e2834db930274b
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-12 23:02:12 +0200

    Create dummy management-components

---
+++ src/app/components/rule-management/rule-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rule-management',
+  templateUrl: './rule-management.component.html',
+  styleUrls: ['./rule-management.component.scss']
+})
+export class RuleManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/user-permission-management/user-permission-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-permission-management',
+  templateUrl: './user-permission-management.component.html',
+  styleUrls: ['./user-permission-management.component.scss']
+})
+export class UserPermissionManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
