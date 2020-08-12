f21f02657409cde85ab441538de9a675f19ac838
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-12 23:02:12 +0200

    Create dummy management-components

---
+++ src/app/components/user-permission-management/user-permission-management.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserPermissionManagementComponent } from './user-permission-management.component';
+
+describe('UserPermissionManagementComponent', () => {
+  let component: UserPermissionManagementComponent;
+  let fixture: ComponentFixture<UserPermissionManagementComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserPermissionManagementComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserPermissionManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
