53921a2a0cd1431dd7f78b50d3b36270acc1b17c
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-12 23:57:14 +0200

    Create side navigation component

---
+++ src/app/components/sidenav-item/sidenav-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-item',
+  templateUrl: './sidenav-item.component.html',
+  styleUrls: ['./sidenav-item.component.scss']
+})
+export class SidenavItemComponent implements OnInit {
+
+  @Input() text: string
+  @Input() component: string
+
+  @ViewChild('itemTemplate', { static: true }) template: TemplateRef<any>
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
